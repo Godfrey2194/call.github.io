41d60ff04ddf3e2a5d271ac90accb7c344b4763a
Kamoti <kamotigodfrey2@gmail.com>
2020-07-09 13:13:28 +0300

    added js

--- JS/contact.js
+++ JS/contact.js
@@ -1,31 +1,11 @@
+function myFunction() {
+  alert("The form was submitted");
+}
+
 function validateForm() {
-    var x = document.forms["form"]["firstname"].value;
-    if (x == "") {
-      alert("Fill all the Detials");
-      return false;
-    }
+  var x = document.forms["form"]["firstname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
   }
-
-  function validateForm() {
-    var x = document.forms["form"]["lastname"].value;
-    if (x == "") {
-      alert("Fill all the Detials");
-      return false;
-    }
-  }
-
-  function validateForm() {
-    var x = document.forms["form"]["emailaddress"].value;
-    if (x == "") {
-      alert("Fill all the Detials");
-      return false;
-    }
-  }
-
-  function validateForm() {
-    var x = document.forms["form"]["comment"].value;
-    if (x == "") {
-      alert("Fill all the Detials");
-      return false;
-    }
-  }
+}
