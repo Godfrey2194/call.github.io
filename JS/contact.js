fcf322440e228df9bd6332ad22ee0fe364244f66
Kamoti <kamotigodfrey2@gmail.com>
2020-07-09 12:37:39 +0300

    removed a function

--- JS/contact.js
+++ JS/contact.js
@@ -2,10 +2,3 @@ function myFunction() {
   alert("The form was submitted");
 }
 
-function validateForm() {
-  var x = document.forms["form"]["firstname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
