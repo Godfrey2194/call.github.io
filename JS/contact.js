5e28415559d1110b1826c54ded3a03c04fad11b9
Kamoti <kamotigodfrey2@gmail.com>
2020-07-14 16:20:08 +0300

    organised the navbar

--- JS/contact.js
+++ JS/contact.js
@@ -1,4 +1,12 @@
-function myFunction() {
-  alert("The form was submitted");
-}
 
+    function myFunction() {
+      alert("Your comment has been successfully submitted. Go Back to the Homepage");
+    }
+
+    function validateForm() {
+      var x = document.forms["form"]["firstname"].value;
+      if (x == "") {
+        alert("Name must be filled out");
+        return false;
+      }
+    }
