6ce9b3f7dd7fa0e83e6b411311e1d9ae711ad82f
Kamoti <kamotigodfrey2@gmail.com>
2020-07-16 10:37:43 +0300

    added colors

--- JS/index.js
+++ JS/index.js
@@ -13,3 +13,4 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
--- JS/work.js
+++ JS/work.js
@@ -7,3 +7,20 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+var y = document.getElementById("work");
+y.style.color = "blue";
+
+var z = document.getElementById("m-work");
+z.style.color = "blue";
+
+var t = document.getElementById("mywork");
+t.style.color = "blue";
+
+var c = document.getElementById("y-work");
+c.style.color = "blue";
+
+var p = document.getElementById("t-work");
+p.style.color = "blue";
+
+var a = document.getElementById("w-work");
+a.style.color = "blue";
